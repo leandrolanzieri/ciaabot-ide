4d7521ffcbc6954d55461566876aa1cc665aefab
leandrolanzieri <lanzierileandro@gmail.com>
2017-09-09 00:06:33 -0300

    Modified createMainFile to return observable.

--- src/app/providers/compiling.service.ts
+++ src/app/providers/compiling.service.ts
@@ -9,6 +9,7 @@ import { NotificationsService } from 'angular2-notifications';
 import * as mkdirp from 'mkdirp';
 import { IdeConfigurationService } from './ide-configuration.service';
 import { ElectronService } from './electron.service';
+import { Observable, Observer } from 'rxjs';
 
 @Injectable()
 export class CompilingService {
@@ -29,99 +30,111 @@ export class CompilingService {
   }
 
   public compileProgram() {
-    this.createMainFile();
-    if (this.workspace) {
-      this.notificationsService.info('Compilando', 'Aguarde unos instantes', {
-        timeOut: 0,
-        maxStack: 1
-      });
+    this.createMainFile().subscribe((success: boolean) => {
+      if (success && this.workspace) {
+        this.notificationsService.info('Compilando', 'Aguarde unos instantes', {
+          timeOut: 0,
+          maxStack: 1
+        });
 
-      childProcess.exec('make -C %PROJECT_PATH% Compilar_proyecto', {
-        env: {
-          PROJECT_PATH: this.workspace.path,
-          PATH: this.electronService.getEnvVariables().PATH + this.electronService.getPathDelimiter() + this.getFormattedPaths(),
-        }
-      }, (err, stdout) => {
-        if (err) {
-          this.notificationsService.error('Problema al compilar', 'Verificar el código');
-          console.error('Error al compilar programa', err);
-          console.info('Salida:', stdout);
-          return;
-        }
-        this.notificationsService.success('Programa compilado', 'Se ha compilado con éxito');
-        console.log('Compilacion finalizada');
-        console.info(stdout);
-      });
-    }
+        childProcess.exec('make -C %PROJECT_PATH% Compilar_proyecto', {
+          env: {
+            PROJECT_PATH: this.workspace.path,
+            PATH: this.electronService.getEnvVariables().PATH + this.electronService.getPathDelimiter() + this.getFormattedPaths(),
+          }
+        }, (err, stdout) => {
+          if (err) {
+            this.notificationsService.error('Problema al compilar', 'Verificar el código');
+            console.error('Error al compilar programa', err);
+            console.info('Salida:', stdout);
+            return;
+          }
+          this.notificationsService.success('Programa compilado', 'Se ha compilado con éxito');
+          console.log('Compilacion finalizada');
+          console.info(stdout);
+        });
+      }
+    });
   }
 
   public downloadProgram() {
-    this.createMainFile();
-    if (this.workspace) {
-      this.notificationsService.info('Descargando', 'Aguarde unos instantes', {
-        timeOut: 0,
-        maxStack: 1
-      });
-
-      childProcess.exec(
-        'make -C %PROJECT_PATH% Compilar_proyecto' +
-        '&& make -C %PROJECT_PATH% Grabar_proyecto_en_flash'
-        , {
-        env: {
-          PROJECT_PATH: this.workspace.path,
-          PATH: this.electronService.getEnvVariables().PATH + this.electronService.getPathDelimiter() + this.getFormattedPaths(),
-        }
-      }, (err, stdout) => {
-        if (err) {
-          this.notificationsService.error('Problema al descargar', 'Verificar la conexión al robot');
-          console.error('Error al compilar programa', err);
-          console.info('Salida:', stdout);
-          return;
-        }
-        this.notificationsService.success('Programa descargado', 'Se ha descargado con éxito');
-        console.log('Descarga finalizada');
-        console.info(stdout);
-      });
-    }
-  }
-
-  private createMainFile(): boolean {
-    let fileDescriptor: number;
-    let file = path.join(this.workspace.path, 'app/src/main.c');
-
-    /* Check if app structure is there */
-    mkdirp(path.join(this.workspace.path, 'app/src'), (err) => {
-      if (err) {
-        this.notificationsService.alert('No se puede crear la carpeta');
-        return false;
-      } else {
-        /* Try to open file */
-        try {
-          fileDescriptor = fs.openSync(file, 'w');
-        } catch (error) {
-          this.notificationsService.alert('No se puede crear el archivo');
-          return false;
-        }
-
-        /* Try to write to file */
-        try {
-          fs.writeFileSync(file, this.workspace.project.code.replace(/\n/, os.EOL));
-        } catch (error) {
-          this.notificationsService.alert('No se puede escribir el archivo');
-          /* Closing the file */
-          fs.closeSync(fileDescriptor);
-          return false;
-        }
-
-        /* Closing the file */
-        fs.closeSync(fileDescriptor);
-        return true;
+    this.createMainFile().subscribe((success: boolean) => {
+      if (success && this.workspace) {
+        this.notificationsService.info('Descargando', 'Aguarde unos instantes', {
+          timeOut: 0,
+          maxStack: 1
+        });
+        childProcess.exec(
+          'make -C %PROJECT_PATH% Compilar_proyecto' +
+          '&& make -C %PROJECT_PATH% Grabar_proyecto_en_flash'
+          , {
+          env: {
+            PROJECT_PATH: this.workspace.path,
+            PATH: this.electronService.getEnvVariables().PATH + this.electronService.getPathDelimiter() + this.getFormattedPaths(),
+          }
+        }, (err, stdout) => {
+          if (err) {
+            this.notificationsService.error('Problema al descargar', 'Verificar la conexión al robot');
+            console.error('Error al compilar programa', err);
+            console.info('Salida:', stdout);
+            return;
+          }
+          this.notificationsService.success('Programa descargado', 'Se ha descargado con éxito');
+          console.log('Descarga finalizada');
+          console.info(stdout);
+        });
       }
     });
   }
 
   /**
+   * Creates the main.c file if it does not exists, then writes
+   * the generated code into it and closes it.
+   * @return Observable<boolean> Success of the async operation
+   */
+  private createMainFile(): Observable<boolean> {
+    let fileDescriptor: number;
+    const file = path.join(this.workspace.path, 'app/src/main.c');
+    return Observable.create((observer: Observer<boolean>) => {
+      /* Check if app structure is there */
+      mkdirp(path.join(this.workspace.path, 'app/src'), (err) => {
+        if (err) {
+          this.notificationsService.alert('No se puede crear la carpeta');
+          observer.next(false);
+          observer.complete();
+        } else {
+          /* Try to open file */
+          try {
+            fileDescriptor = fs.openSync(file, 'w');
+          } catch (error) {
+            this.notificationsService.alert('No se puede crear el archivo');
+            observer.next(false);
+            observer.complete();
+          }
+
+          /* Try to write to file */
+          try {
+            fs.writeFileSync(file, this.workspace.project.code.replace(/\n/, os.EOL));
+          } catch (error) {
+            this.notificationsService.alert('No se puede escribir el archivo');
+            /* Closing the file */
+            fs.closeSync(fileDescriptor);
+            observer.next(false);
+            observer.complete();
+          }
+
+          /* Closing the file */
+          fs.closeSync(fileDescriptor);
+          observer.next(true);
+          observer.complete();
+        }
+      });
+    });
+  }
+
+  /**
    * Returns the configured paths as a semicolon-separated string.
+   * @return string Formatted paths
    */
   private getFormattedPaths(): string {
     let retValue = '';
